3290fc85e1013ddfe7809510d1df4073ee281f3a
ydnaCandy <keynomori@gmail.com>
2024-11-22 15:43:05 +0900

    feat: エディットカラム取得関数の通常関数変更と返り値の辞書名と記述方法を変更

--- sources/Pleasanter/python/Module/pleasanter_api.py
+++ sources/Pleasanter/python/Module/pleasanter_api.py
@@ -45,15 +45,14 @@ class PleasanterConnector:
                 # レスポンスをjson形式に変換
                 data: dict = request_post.json()
                 # 返り値を作成
-                response_dict: dict = {
+                return {
                     "Result": True,
                     "ResponseData": data["Response"]["Data"]
                 }
-                return response_dict
 
         # タイムアウト
         except requests.exceptions.ConnectTimeout as e:
-            error_msg: dict = {
+            return {
                 "Result": False,
                 "ErrorMsg": {
                     "ErrorType":str(type(e)),
@@ -61,33 +60,30 @@ class PleasanterConnector:
                 }
             }
 
-            return error_msg
 
         # 接続エラー
         except requests.exceptions.ConnectionError as e:
-            error_msg: dict = {
+            return {
                 "Result": False,
                 "ErrorMsg": {
                     "ErrorType":str(type(e)),
                     "Message":"Connection Error"
                 }
             }
-            return error_msg
 
         # 想定外エラー
         except Exception as e:
-            error_msg: dict = {
+            return {
                 "Result": False,
                 "ErrorMsg": {
                     "ErrorType":str(type(e)),
                     "Message":"Unexcepted Error"
                 }
             }
-            return error_msg
 
 
 
-    def _get_columns_in_edit_tab(self, site_id: str) -> dict:
+    def get_columns_in_edit_tab(self, site_id: str) -> dict:
         """Return Dict
         Explain:
             - This function retrieves and processes site information required for using GridColumn in edit tab.
@@ -101,7 +97,6 @@ class PleasanterConnector:
         # pleasanter標準のカラムで使用するカラムを指定
         default_cols: list = ['CreatedTime', 'UpdatedTime']
 
-        # try:
         # APIにリクエストするデータを作成
         payload: dict = self.paylaod
 
@@ -118,38 +113,33 @@ class PleasanterConnector:
                 col_list: list = response_dict["ResponseData"]["SiteSettings"]["EditorColumnHash"]["General"]
                 # デフォルトカラムで使用するカラム名を追加
                 col_list.extend(default_cols)
-
-                result_dict: dict = {
+                # 結果の出力
+                return {
                     "Result": True,
-                    "Data": {
+                    "ResponseData": {
                         "EditColsList": col_list
                     }
                 }
 
-                return result_dict
-
-
             elif response_dict["ResponseData"]["ReferenceType"] == "Sites":
                 # ディレクトリサイトがsite_idとして指定されたらNGを返す
-                result_dict: dict = {
+                return {
                     "Result": False,
                     "ErrorMsg": {
                         "ErrorType": "Uncorrect site id.",
                         "Message": "This id is directly site id."
                     }
                 }
-                return result_dict
 
             else:
                 # 想定外のエラー
-                result_dict: dict = {
+                return {
                     "Result": False,
                     "ErrorMsg": {
                         "ErrorType": "Unexpected.",
                         "Message": "Successfully response. But Unexpected ReferenceType."
                     }
                 }
-                return result_dict
 
         # リクエスト送信時のエラー
         else:
@@ -159,17 +149,18 @@ class PleasanterConnector:
             self,
             site_id: str,
             record_id: str,
-            get_edit_cols: bool = True
+            grid_columns: Optional[list] = None,
         ) -> dict:
         """return dict(result request and data)
         Args:
-        - get_edit_cols: 
+        - grid_columns: 
             - True: columns in edit tab
             - False: columns in grid(view) tab
         """
-
+        # site_idとrecord_idに同じ値がセットされた
         if site_id == record_id:
-            result_dict: dict = {
+
+            return  {
                 "Result": False,
                 "ErrorMsg": {
                     "ErrorType": "The ID is invalid.",
@@ -184,22 +175,10 @@ class PleasanterConnector:
             "View": {
                 # Pleasanter上の表示名で取得
                 "ApiDataType": "KeyValues",
-                
+                # グリッドカラムの指定
+                "GridColumns": grid_columns
             }
         })
-
-        # エディットカラムを取得
-        if get_edit_cols:
-            request_get_cols: dict = self._get_columns_in_edit_tab(site_id=site_id)
-            
-            # エディットカラムを取得するためのリクエストの処理
-            if request_get_cols["Result"]:
-                # エディットカラムで指定したカラムをpayloadの条件に追加
-                payload["View"]["GridColumns"] = request_get_cols["Data"]["EditColsList"]
-
-            else:
-                # エディットカラム取得時のリクエストのエラー
-                return response_dict
 
         # URLを作成
         url = self.pl_addr + "api/items/" + str(record_id) + "/get"
@@ -216,9 +195,10 @@ class PleasanterConnector:
             if isinstance(response_dict["ResponseData"], list) and len(response_dict["ResponseData"]) == 1:
                 result_dict: dict = {
                     "Result": True,
-                    "Data": response_dict["ResponseData"]
+                    "ResponseData": response_dict["ResponseData"]
                 }
                 return result_dict
+
             # リスト以外もしくはレコードIDが複数行ある
             else:
                 result_dict: dict = {
